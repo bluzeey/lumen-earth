91473770ebd1b259ae15465d8843bb0428e85d15
Sahil Maheshwari <sahilm1711@gmail.com>
2025-08-04 13:43:16 +0530

    refactor: move auth views to accounts app

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from main import views, auth_views
+from main import views
+from accounts import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
